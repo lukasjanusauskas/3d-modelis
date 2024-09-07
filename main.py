1ba4f9a5a9a74dfb4794ca9491a47976c03578d9
lukasjanusauskas <lukasjanusauskaslj@gmail.com>
2024-09-07 17:29:19 +0300

    Sukurtas neoptimizuotas 3D modelio generavimas

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
-import time
 from numba import jit, vectorize, complex64, int16
 
-GRID_DEF = 2 ** 7
-MAX_ITER = 100
+COLOR_GREEN = (106, 123, 106)
+COLOR_RED = (123, 106, 106)
+
+GRID_DEF = 2 ** 11
+MAX_ITER = 200
+MAX_HEIGHT = 0.1
 
 @jit(nopython=True)
 def init_grid(grid_def: int) -> np.array:
@@ -31,9 +34,47 @@ def iterate(point: np.complex64):
 
     return output
 
+def print_dimensions(file):
+    vertices = (GRID_DEF + 1) ** 2
+    sides = GRID_DEF ** 2
+
+    print(f"{vertices} {sides} 0", file=file)
+
+def print_vertices(grid, output, file):
+    for compl, out in zip(np.ravel(grid), np.ravel(output)):
+        print(f"{np.real(compl)} {np.imag(compl)} {np.sin(out / MAX_ITER * MAX_HEIGHT * np.pi/2)}", file=file)
+
+def avg_height(output, vertices):
+    return np.mean([output[i] for i in vertices])
+
+def print_side(output, vertices, file):
+    print(4, *vertices, end=" ", file=file)
+
+    avg_h = avg_height(output, vertices)
+    factor = 1 - (avg_h / MAX_ITER)
+    color = (int(c1*factor + c2*(1-factor))
+             for c1, c2 in zip(COLOR_GREEN, COLOR_RED))
+
+    print(*color, file=file)
+
+def print_all_sides(output, file):
+    for i in range(GRID_DEF):
+        for j in range(GRID_DEF):
+            vertices = (i * (GRID_DEF + 1) + j,
+                        i * (GRID_DEF + 1) + j + 1,
+                        i * (GRID_DEF + 1) + j + 1 + GRID_DEF,
+                        i * (GRID_DEF + 1) + j + GRID_DEF)
+            print_side(np.ravel(output), vertices, file)
+
 def main():
     grid = init_grid(GRID_DEF)
     output = iterate(grid)
+
+    with open("out1.off", "w") as f:
+        print("OFF", file=f)
+        print_dimensions(grid, f)
+        print_vertices(grid, output, f)
+        print_all_sides(output, f)
 
 if __name__ == "__main__":
     main()
